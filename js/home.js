35e1fef2f26e42b7e54395c382a1ee5a742333f3
Tom Gobich <fallofmars@gmail.com>
2016-10-19 09:17:15 -0400

    added missing class to comic listing on home page

--- js/home.js
+++ js/home.js
@@ -166,7 +166,7 @@ $(document).ready(function()
 			// Load HTML block into variable
 			comicElement +=
 			`
-				<div data-id="${comic.id}" class="col-xs-4 col-md-2">
+				<div data-id="${comic.id}" class="col-xs-4 col-md-2 comic-listing">
 					<img class="comic-image" src="${comic.image}" alt="${comic.title}">
 					<h4 class="comic-title">${comic.title}</h4>
 					<p class="comic-price">$${comic.price}</p>
